7a81628aa9a8646b8ddd0e7f35bd6738f7ae123e
Luis <luis.rocha@alumnos.upm.es>
2021-10-19 13:48:36 +0200

    commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const methodOverride = require('method-override');
+
 const userRoutes = require('./routes/laptopRoutes');
+const tasksRoutes =require('./routes/tasksRoutes')
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
