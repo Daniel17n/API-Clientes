b70e66fb6f03b18b8fe40f8b205ad18c3ea02145
Luis <luis.rocha@alumnos.upm.es>
2021-10-25 11:20:52 +0200

    commit

--- models/userModel.js
+++ models/userModel.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
-const {pbkdf2Sync, randomBytes} = require('crypto');
+const { pbkdf2Sync, randomBytes } = require('crypto');
 
 const userSchema = new Schema({
   name: { type: String, required: true },
   surname: { type: String, required: true },
   email: { type: String, required: true },
-  password: { type: String, required: true},
+  password: { type: String, required: true },
   genre: { type: String, required: true },
   organization: { type: String },
 });
